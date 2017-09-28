447f0927e7a7d1508c5c0bfe689e989cc8f070f2
algoridc <algoridc@gmail.com>
2017-09-28 19:56:53 +0200

    cambios

--- ejemplo/src/main/java/dneud/ejemplo/App.java
+++ ejemplo/src/main/java/dneud/ejemplo/App.java
@@ -1,5 +1,7 @@
 package dneud.ejemplo;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola Mundo!" );
+
+        System.out.println( "Hola Mundo!"+teclado("escribe un entero") );
+    }
+    public static int teclado(String txt) {
+        //este modulo es para no tener que repetir el scanner
+        //txt es el texto que quiero mostrar en pantalla
+        System.out.println(txt);
+        Scanner leer= new Scanner(System.in);
+       int n=leer.nextInt();
+       return n;
     }
 }
+
